ccb57b9afc8476f9af55ad8a758908a47dc75547
Fawad Khan <fawad.khan@tib.eu>
2021-10-27 17:01:32 +0200

    Reverting the CD Change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
     render() {
-        return <div>HOME PAGE TEST</div>;
+        return <div>HOME PAGE</div>;
     }
 }
