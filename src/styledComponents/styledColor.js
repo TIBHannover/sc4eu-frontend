4037b0f2aeebce080091862d37cb1a95389520e5
Chavadanilesh <Nilesh.chavada@tib.eu>
2023-04-06 15:52:47 +0200

    Two new styled files added

---
+++ src/styledComponents/styledColor.js
@@ -0,0 +1,20 @@
+const PRIMARY = {
+    lighter: '#F7FBFC',
+    light: '#D6E6F2',
+    lightMain: '#92bdff',
+    main: '#B9D7EA',
+    dark: '#769FCD'
+};
+const SECONDARY = {
+    dark: '#536b78',
+    darker: '#2f3d45',
+    link: '#0000EE'
+};
+
+const TEXTCOLOR = '#4d5b7c';
+
+export const colorStyled = {
+    PRIMARY: PRIMARY,
+    SECONDARY: SECONDARY,
+    TEXTCOLOR: TEXTCOLOR
+};
