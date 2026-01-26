TIBHannover/sc4eu-frontend

commit 1d34d81b1476e02a4e5b45eed0e296591ede6a82

Read token from cookie instead of Authorization header
Verify token function, which is called to check if user is authenticated by searching for existing token previously used user-defined Authorization header. Issue with this, is when a reload page was hit, Authorization header and all other manually added headers are removed, and user is said to be not authenticated.

We already use cookies, and token itself is already present in a cookie, moreover even Authorization header itlsef is populated from the cookie. I have switched verify token function to check the token presence in a cookie.

server/veryfyToken.js
@@ -1,13 +1,11 @@
 module.exports = function(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    if (bearerHeader) {
-        console.log('>>> WE HAVE A TOKEN  ');
-        const bearer = bearerHeader.split(' ');
-        req.token = bearer[1];
-        next();
-    } else {
-        // Forbidden
+    const token = req.cookies.token;
+    if (!token) {
         console.log('WE DONT HAVE A TOKEN HERE oO ');
         res.send(JSON.stringify({ error: 'No Token Provided' }));
     }
+
+    req.token = token;
+    next();
+
 };

commit 361fc8029cd5e3bd64431148ed808d6245e9e9e9

Make verify token check for Authorization header and cookies
For reasons unclear to me yet, cookies are not set during the first authentication. Previous commit 1d34d81 broke authentication, server couldn't see available cookies, due to the differences between client and server cookies.

To prevent error, described in issue 466, I decided to check for the Authorization header first - that would work in most cases of first going to URL, and afterward check for the cookie, if Authorization header is not found - that prevents page refresh no token issue

server/veryfyToken.js
@@ -1,11 +1,23 @@
+const { Cookies } = require('react-cookie');
+
 module.exports = function(req, res, next) {
-    const token = req.cookies.token;
-    if (!token) {
-        console.log('WE DONT HAVE A TOKEN HERE oO ');
-        res.send(JSON.stringify({ error: 'No Token Provided' }));
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader) {
+        console.log('>>> WE HAVE A TOKEN  ');
+        const bearer = bearerHeader.split(' ');
+        req.token = bearer[1];
+        return next();
     }
 
-    req.token = token;
-    next();
+    const cookies = new Cookies();
+    const token = cookies.get('token');
+    if (token) {
+        req.token = token;
+        return next();
+    }
 
+    if (!token) {
+        console.log('WE DONT HAVE A TOKEN HERE oO ');
+        return res.send(JSON.stringify({ error: 'No Token Provided' }));
+    }
 };

src/components/UserViewDetails.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Col, Container, FormGroup, Input, Label, Button } from 'reactstrap';
-// import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-// import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { StyledGravatar } from '../styledComponents/styledComponents';