e80fbb8cdbee23106787618a8243ea6300bbde11
vaninm <mark.vanin@tib.eu>
2026-03-05 14:20:15 +0100

    Add push event listener to service worker. Allow service worker to register in dev environment

--- src/service-worker.js
+++ src/service-worker.js
@@ -69,4 +69,53 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('push', event => {
+  console.log('Push event received:', event);
+  if (Notification.permission !== 'granted') {
+    console.error('Notification permission not granted');
+    return;
+  }
+  let notificationData = {
+    title: 'New Notification',
+    body: 'You have a new notification',
+    data: {
+      url: '/ocp/'
+    }
+  };
+
+  try {
+    if (event.data) {
+      const payload = event.data.json();
+      console.log('Payload:', payload);
+
+      notificationData = {
+        title: payload.title || notificationData.title,
+        body: payload.body || notificationData.body,
+        icon: payload.icon || notificationData.icon,
+        badge: payload.badge || notificationData.badge,
+        tag: payload.type || 'default',
+        data: {
+          url: payload.url || notificationData.data.url,
+          vote_id: payload.vote_id,
+          type: payload.type
+        },
+        requireInteraction: true,
+      };
+    }
+  } catch (error) {
+    console.error('Error parsing push notification data:', error);
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(notificationData.title, {
+      body: notificationData.body,
+      icon: notificationData.icon,
+      badge: notificationData.badge,
+      tag: notificationData.tag,
+      data: notificationData.data,
+      requireInteraction: notificationData.requireInteraction,
+    })
+  );
+});
+
 // Any other custom service worker logic can go here.
