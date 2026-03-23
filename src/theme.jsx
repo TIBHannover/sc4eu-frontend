1a5ba2d4fb20156c7b9897c7f130a78cf035607c
vaninm <mark.vanin@tib.eu>
2026-03-23 14:27:51 +0100

    Default theme colours update

--- src/theme.jsx
+++ src/theme.jsx
@@ -4,13 +4,20 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#1976d2' // Customize primary color
+            main: '#1976d2',
+            lightMain: '#92bdff',
+            lighter: '#F7FBFC',
+            light: '#D6E6F2',
+            dark: '#769FCD'
         },
         secondary: {
-            main: '#dc004e' // Customize secondary color
+            main: '#dc004e',
+            dark: '#536b78',
+            darker: '#2f3d45',
+            link: '#0000EE'
         },
         background: {
-            default: '#f5f5f5' // Customize background color
+            default: '#f5f5f5'
         }
     },
     typography: {
@@ -18,7 +25,7 @@ const theme = createTheme({
             fontSize: '1rem'
         }
     },
-    spacing: 8 // Default spacing
+    spacing: 8
 });
 
 export default theme;
