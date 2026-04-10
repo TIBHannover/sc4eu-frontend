6b907d5081f31e2a3a7bebfcfca787f1d074f587
vaninm <mark.vanin@tib.eu>
2026-04-10 14:15:35 +0200

    Update oauth config using 'vocab' in URL path

--- server/config/oauth.config.js
+++ server/config/oauth.config.js
@@ -4,32 +4,32 @@ module.exports = {
         tokenURL: process.env.OAUTH2_TOKEN_URL,
         clientID: process.env.OAUTH2_CLIENT_ID,
         clientSecret: process.env.OAUTH2_CLIENT_SECRET,
-        callbackURL: `${process.env.CALLBACK_URL}/ocp/oauth/oauth2/callback`,
+        callbackURL: `${process.env.CALLBACK_URL}/vocab/oauth/oauth2/callback`,
         scope: ['profile', 'email'],
         state: true
     },
     gitlab: {
         clientID: process.env.GITLAB_CLIENT_ID,
         clientSecret: process.env.GITLAB_CLIENT_SECRET,
-        callbackURL: `${process.env.CALLBACK_URL}/ocp/oauth/gitlab/callback`
+        callbackURL: `${process.env.CALLBACK_URL}/vocab/oauth/gitlab/callback`
     },
     github: {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `${process.env.CALLBACK_URL}/ocp/oauth/github/callback`
+        callbackURL: `${process.env.CALLBACK_URL}/vocab/oauth/github/callback`
     },
     sap: {
         authorizationURL: process.env.SAP_AUTHORIZATION_URL_FOR_LOGIN,
         tokenURL: process.env.SAP_TOKEN_URL_FOR_LOGIN,
         clientID: process.env.SAP_CLIENT_ID_FOR_LOGIN,
         clientSecret: process.env.SAP_CLIENT_SECRET_FOR_LOGIN,
-        callbackURL: `${process.env.CALLBACK_URL}/ocp/oauth/sap/callback`,
+        callbackURL: `${process.env.CALLBACK_URL}/vocab/oauth/sap/callback`,
         scope: ['openid', 'email', 'profile']
     },
     google: {
         clientID: process.env.GOOGLE_CLIENT_ID_FOR_LOGIN,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET_FOR_LOGIN,
-        callbackURL: `${process.env.CALLBACK_URL}/ocp/oauth/google/callback`,
+        callbackURL: `${process.env.CALLBACK_URL}/vocab/oauth/google/callback`,
         scope: ['profile', 'email']
     }
 };
