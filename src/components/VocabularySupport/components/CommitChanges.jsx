b1504c01d3da0a287d56aa016ed5e18eb2af829d
vaninm <mark.vanin@tib.eu>
2026-05-11 17:55:16 +0200

    Throw exception if Git commit to terms or discussion file failed
    If term deletion was successful vocabulary table will be notified with a new CommitChanges prop onSuccess. New request deleteTermVotes will be then send to a backend to remove existing consensus connected to a removed term/-s.
    Response object is a list of removed - consensus that were removed from database, failed - consensuses, which were not removed with a fail reason.

--- src/components/VocabularySupport/components/CommitChanges.jsx
+++ src/components/VocabularySupport/components/CommitChanges.jsx
@@ -1,29 +1,53 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Alert } from '@mui/material';
 import TextField from '@material-ui/core/TextField';
 import React, { useState } from 'react';
 import { commitChanges } from '../utils/CommitChanges';
 import PropTypes from 'prop-types';
 import { useQueryClient } from '@tanstack/react-query';
 import { usePushNotifications } from '../../../hooks/usePushNotifications';
+import { colorStyled } from 'styledComponents/styledColor';
 
-const CommitChanges = ({ refetch, openCommit, setOpenCommit, setHasUncommittedChanges, user }) => {
+const CommitChanges = ({ refetch, openCommit, setOpenCommit, setHasUncommittedChanges, user, onSuccess, onFail }) => {
     const [commitMessage, setCommitMessage] = useState('');
+    const [commitError, setCommitError] = useState('');
     const queryClient = useQueryClient();
     const { notifyAddRemoveTerm } = usePushNotifications(user);
     const handleCommitMessageChange = event => {
         setCommitMessage(event.target.value);
     };
     const handleCommit = async () => {
-        await commitChanges(queryClient, commitMessage);
-        await notifyAddRemoveTerm();
-        setHasUncommittedChanges(false);
-        setOpenCommit(false);
+        try {
+            await commitChanges(queryClient, commitMessage);
+            await notifyAddRemoveTerm();
+            onSuccess?.();
+            setHasUncommittedChanges(false);
+            setOpenCommit(false);
+        } catch (e) {
+            onFail?.(e);
+            setCommitError('Failed to save changes. Please try again.');
+        }
     };
+
     return (
         <Dialog open={openCommit} onClose={() => setOpenCommit(false)} maxWidth="xl">
             <DialogTitle>Save Changes</DialogTitle>
             <DialogContent>
                 <DialogContentText>Please enter a short description of your changes</DialogContentText>
+                {commitError && (
+                    <Alert
+                        severity="error"
+                        sx={{
+                            mb: 1,
+                            backgroundColor: colorStyled.errorContainer,
+                            color: colorStyled.error,
+                            '& .MuiAlert-icon': {
+                                color: colorStyled.error
+                            }
+                        }}
+                    >
+                        {commitError}
+                    </Alert>
+                )}
                 <TextField
                     autoFocus
                     margin="dense"
@@ -58,7 +82,9 @@ CommitChanges.propTypes = {
     refetch: PropTypes.func.isRequired,
     openCommit: PropTypes.bool.isRequired,
     setOpenCommit: PropTypes.func.isRequired,
-    setHasUncommittedChanges: PropTypes.func.isRequired
+    setHasUncommittedChanges: PropTypes.func.isRequired,
+    onSuccess: PropTypes.func,
+    onFail: PropTypes.func
 };
 
 export default CommitChanges;
